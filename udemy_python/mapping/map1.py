6f9542eab6238a91db16b688525fd41ce291867e
eobrie16 <late.for.lunch@gmail.com>
2020-05-06 19:38:09 -0400

    Updates from Udemy Python course

--- udemy_python/mapping/map1.py
+++ udemy_python/mapping/map1.py
@@ -10,22 +10,35 @@ def get_color(elevation):
         return "red"
 
 data = pandas.read_csv("Volcanoes.txt")
-map = folium.Map(location=[40, -90], zoom_start=5)
+map = folium.Map(location=[40, -99], zoom_start=5, tiles="Stamen Terrain")
+# map = folium.Map(location=[40, -99], zoom_start=5)
+fg = folium.FeatureGroup(name="my map")
+
+html = """<h4>Volcano information:</h4>
+Height: %s m
+"""
+
 fgv = folium.FeatureGroup(name="Volcanoes")
 
 for lat, lon, el in zip(list(data['LAT']), list(data['LON']), list(data['ELEV'])):
-    #fg.add_child(folium.Marker(location=[lat,lon], popup=str(el), 
-    #icon=folium.Icon(icon='ok', icon_color='white', color=get_color(el))))
+    iframe = folium.IFrame(html=html % str(el), width=200, height=100)
     c = get_color(el)
-    fgv.add_child(folium.CircleMarker(location=[lat,lon], radius=5, color=c, fill_color=c,
-                 popup=str(el)+"m", fill_opacity=0.75))
+    # fg.add_child(folium.Marker(location=[lat,lon], popup=folium.Popup(iframe),
+    #              icon=folium.Icon(icon='ok', icon_color='white', color=c)))
+    
+    fgv.add_child(folium.CircleMarker(location=[lat,lon], radius=5, color='black', fill_color=c,
+                 popup=folium.Popup(iframe), fill_opacity=0.75))
           
 fgp = folium.FeatureGroup(name="Population")       
-fgp.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig'), 
+fgp.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
     style_function=lambda x:{'fillColor':'green' if x['properties']['POP2005'] < 10e6 
-    else 'orange' if 10e6 <= x['properties']['POP2005'] < 20e6 else 'red'}))
+    else 'orange' if 10e6 <= x['properties']['POP2005'] < 50e6 else 'red'}))
       
 map.add_child(fgv)
 map.add_child(fgp)
 map.add_child(folium.LayerControl())
-map.save("Map1.html")
+
+
+# fg.add_child(folium.Marker(location=[35.9, -97.6], popup="Marker loc", icon=folium.Icon(color='green')))
+# map.add_child(fg)
+map.save("Map2.html")
